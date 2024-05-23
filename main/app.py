5000a6f82574c24411060d33c4f2a62c6dd916dc
RobertoAM1 <robertoandrei.miron@gmail.com>
2024-05-23 17:12:16 +0200

    Setup frontend

--- main/app.py
+++ main/app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,send_from_directory
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='frontend/build', static_url_path='')
 
 # URI di connessione al cluster cloud
 uri = os.getenv('MONGO_URI')
@@ -21,7 +22,8 @@ collection2 = db['world_cup_matches']
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return send_from_directory(app.static_folder, 'index.html')
+
 
 if __name__ == '__main__':
     app.run(host="localhost", port=8080, debug=True)
